74779fa4fb0e153a428c0ca69c44d646770439e1
jeisacarvalho <jeisasilva1823@gmail.com>
2023-04-13 22:05:21 -0300

    react

--- lista-de-tarefa/src/App.jsx
+++ lista-de-tarefa/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
-import { ReactDOM } from "react"
+import { ReactDOM } from "react-dom/client"
 import TodoList from "./TodoList"
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StricMode>
     <TodoList />
-  </React.StrictMode>,
+  </React.StricMode>
 )
+
--- lista-de-tarefa/src/TodoList.jsx
+++ lista-de-tarefa/src/TodoList.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import './TodoList.css';
 
 function TodoList() {
-    return (
-    <h1>Olá mundo!</h1>
-    )
 
+    const [Lista, setLista] = useState([]);
+    const [novoItem, setNovoItem] = useState("");
+
+    function adicionarItem(form) {
+        form.preventDafault();
+        if (!novoItem) {
+            return;
+        }
+        setLista([...lista, {text: novoItem, iscompleted:false}])
+    }
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+
+            <form onSubmit={adicionarItem}>
+
+                <input type="text" 
+                value={novoItem}
+                onChange={(e) => {setNovoItem(e.target.value)}}
+                placeholder="Adicionar uma tarefa"/>
+                <button className="add" type="submit">Add</button>
+
+            </form>
+            <div className="listaTarefa">
+                <div className="item"> 
+                <span>Tarefa de exemplo</span>
+                <button className="del" >Deletar</button>
+            </div>
+            <div className="item competo">
+                <span>Tarefa de exemplo</span>
+                <button className="del" >Deletar</button>
+
+            </div>
+            <div>
+                <button className="deletarall" >Deletar Todas</button>
+            </div>
+        </div>
+        </div>
+    )
 }
 
 export default TodoList
