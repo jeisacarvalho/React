293c4cd8ae0471d47997b5921715ced5a98f777d
jeisacarvalho <jeisasilva1823@gmail.com>
2023-04-27 00:26:22 -0300

    react

---
+++ styled-components/styled/src/App.jsx
@@ -0,0 +1,6 @@
+
+function App() {
+  return <div className="App">Front Beginners</div>
+}
+
+export default App
---
+++ styled-components/styled/src/components/Button.style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StyledButton = styled`
+/* ...colocar nossos estilos */
+
+background-color: red;
+width: 100px;
+
+
+
+`
