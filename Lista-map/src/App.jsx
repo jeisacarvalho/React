3956069612c3da68e28c19163180e532b9983704
jeisacarvalho <jeisasilva1823@gmail.com>
2023-05-11 11:23:52 -0300

    react

--- Lista-map/src/App.jsx
+++ Lista-map/src/App.jsx
@@ -1,3 +1,39 @@
+// Manipulando elememtos de formulário com React
+import { useState } from "react"
+
+export default function App() {
+
+  const [nome,setNome]=useState('')
+  const [carro,setCarro]=useState('')
+  return (
+    <>
+    <label>Digite seu Nome</label>
+    <input type="text" name="fnome"  
+    value={nome}
+    onChange={(e)=>setNome(e.target.value)}
+    
+    />
+    <p>Nome digitado: {nome}</p>
+
+    <label>Selecione um carro</label>
+    <select value={carro} onChange={(e)=>setCarro(e.target.value)}>
+      <option value="HRV>">HRV</option>
+      <option value="Golf>">Golf</option>
+      <option value="Cruze>">Cruze</option>
+      <option value="Argo>">Argo</option>
+    </select>
+    <p>Carro selecionado {carro}</p>
+    </>
+  )
+}
+
+
+
+
+
+// Trabalhando com Lista em React usando a função MAP
+
+/*
 export default function App() {
 
   const carros=['HRV','Golf','Focus','Cruze','Argo']
@@ -15,3 +51,4 @@ export default function App() {
     </>
   )
 }
+*/
